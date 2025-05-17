82df5074aa61c328836c5e8c0c585cd6deb4c455
Sreedevi <ega.sreedeviajith@gmail.com>
2025-05-17 22:22:51 +0300

    Ex 1.4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,4 +1,4 @@
-// Ex 1.3
+//Exercise 1.4
 const Header = ({ course }) => <h1>{course}</h1>;
 
 const Part = ({ part }) => (
@@ -14,26 +14,27 @@ const Content = ({ parts }) => (
 );
 
 const Total = ({ parts }) => (
-  <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+  <p>
+    Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+  </p>
 );
 
 const App = () => {
   const course = 'Half Stack application development';
-
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  };
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  };
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  };
-
-  const parts = [part1, part2, part3];
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10,
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7,
+    },
+    {
+      name: 'State of a component',
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
